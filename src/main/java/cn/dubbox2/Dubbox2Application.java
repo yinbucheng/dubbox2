1c4e018e21b49c146bdeac1832d9a728066f1923
yinchong <yin.chong@intellif.com>
2018-12-23 20:51:35 +0800

    Initial commit

---
+++ src/main/java/cn/dubbox2/Dubbox2Application.java
@@ -0,0 +1,14 @@
+package cn.dubbox2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dubbox2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dubbox2Application.class, args);
+    }
+
+}
+
